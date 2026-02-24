6d519c650415bf53a31b25485c0de801b952e242
M-y-les <mylesfreelin21@gmail.com>
2026-02-23 22:20:03 -0500

    Launcher tweak

--- src/main/java/org/example/oop_and_javafx_assignment/Launcher.java
+++ src/main/java/org/example/oop_and_javafx_assignment/Launcher.java
@@ -1,9 +1,7 @@
 package org.example.oop_and_javafx_assignment;
 
-import javafx.application.Application;
-
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
+        javafx.application.Application.launch(MainApplication.class, args);
     }
 }
