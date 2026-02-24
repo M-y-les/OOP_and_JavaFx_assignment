677b5c26a0e25b1d0001674397af96f4ad03a082
M-y-les <mylesfreelin21@gmail.com>
2026-02-23 21:55:23 -0500

    module info change

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module org.example.oop_and_javafx_assignment {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.graphics;
 
     opens org.example.oop_and_javafx_assignment to javafx.fxml;
     exports org.example.oop_and_javafx_assignment;
